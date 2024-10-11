0b452033f7157a3619f8e5872eef6d17018e1c07
Huy Le <ing.huyle@gmail.com>
2024-10-11 13:52:35 +0200

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import './styles/App.scss';
-import $ from "jQuery";
+import $ from 'jQuery';
 import initialValue from './initialValue';
 import { marked } from 'marked';
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const App = () => {
   const [value, setValue] = useState(initialValue);
   
   const handleClickEditor = () => {
-    $(".preview-wrapper").toggleClass("display");
-    $("#editor").toggleClass("min-height-editor");
-    $("#editor").toggleClass("full-height");
-    $("#editor").toggleClass("resize-vertical");
-    $("#editor").toggleClass("resize-none");
-    $(".icon").toggleClass("minimize");
-    $(".icon").toggleClass("maximize");
+    $('.preview-wrapper').toggleClass('display');
+    $('#editor').toggleClass('min-height-editor');
+    $('#editor').toggleClass('full-height');
+    $('#editor').toggleClass('resize-vertical');
+    $('#editor').toggleClass('resize-none');
+    $('.icon').toggleClass('minimize');
+    $('.icon').toggleClass('maximize');
   }
 
   const handleClickPreview = () => {
-    $(".editor-wrapper").toggleClass("display");
-    $("#preview").toggleClass("min-height-preview");
-    $("#preview").toggleClass("full-height");
-    $(".icon").toggleClass("minimize");
-    $(".icon").toggleClass("maximize");
+    $('.editor-wrapper').toggleClass('display');
+    $('#preview').toggleClass('min-height-preview');
+    $('#preview').toggleClass('full-height');
+    $('.icon').toggleClass('minimize');
+    $('.icon').toggleClass('maximize');
   }
 
   const handleChange = (event) => {
@@ -30,24 +30,24 @@ const App = () => {
   }
 
   useEffect(() => {
-    $("#preview").html(marked.parse(value));
+    $('#preview').html(marked.parse(value));
   }, [value]);
 
   return (
-    <div className="app-wrapper">
-      <div className="editor-wrapper">
-        <div className="titlebar">
+    <div className='app-wrapper'>
+      <div className='editor-wrapper'>
+        <div className='titlebar'>
           <div>Editor</div>
-          <div className="icon maximize" onClick={handleClickEditor}></div>
+          <div className='icon maximize' onClick={handleClickEditor}></div>
         </div>
-        <textarea id="editor" className="resize-vertical min-height-editor" value={value} onChange={handleChange}></textarea>
+        <textarea id='editor' className='resize-vertical min-height-editor' value={value} onChange={handleChange}></textarea>
       </div>
-      <div className="preview-wrapper">
-        <div className="titlebar">
+      <div className='preview-wrapper'>
+        <div className='titlebar'>
           <div>Preview</div>
-          <div className="icon maximize" onClick={handleClickPreview}></div>
+          <div className='icon maximize' onClick={handleClickPreview}></div>
         </div>
-        <div id="preview" className="min-height-preview"></div>
+        <div id='preview' className='min-height-preview'></div>
     </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './styles/index.scss'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './styles/index.scss';
 
 createRoot(document.getElementById('root')).render(
   <App />
-)
+);
